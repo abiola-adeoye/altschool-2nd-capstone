c6f15d7dd3e5fef5514d41153d2ccdc5e3c22bdd
abiola-adeoye <slmadeoye@gmail.com>
2023-09-21 12:13:28 +0100

    updating project

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ The default mode of the program is to run in test mode, when it does this, it'll
 the web drowser. When the goal is to scrape all the data on the ministry of health webpage pass in the argument value of
 'False' to the class instance assides the website, this will scrape all the data and hide the web browser display
 '''
-import transform_script
-from extraction_script import MHScrapper
+from src import transform_script
+from src.extraction_script import MHScrapper
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -29,16 +29,16 @@ for key in result:
     data_table_df = pd.DataFrame.from_records(result[key])
     data_table_df.to_csv(file_path+f"{key}.csv", index=False, sep=";")      # save extracted data
     if key =="page_rows":
-        transform_script.clean_load_page_rows(file_path+f"{key}.csv", transform_script.dtype_pages, engine)
+        transform_script.clean_load_page_rows(file_path + f"{key}.csv", transform_script.dtype_pages, engine)
     elif key == "identifiers":
-        transform_script.clean_load_identifiers(file_path+f"{key}.csv", transform_script.dtype_identifiers, engine)
+        transform_script.clean_load_identifiers(file_path + f"{key}.csv", transform_script.dtype_identifiers, engine)
     elif key == "locations":
-        transform_script.clean_load_locations(file_path+f"{key}.csv", transform_script.dtype_locations, engine)
+        transform_script.clean_load_locations(file_path + f"{key}.csv", transform_script.dtype_locations, engine)
     elif key == "contacts":
-        transform_script.clean_load_contacts(file_path+f"{key}.csv", transform_script.dtype_contacts, engine)
+        transform_script.clean_load_contacts(file_path + f"{key}.csv", transform_script.dtype_contacts, engine)
     elif key == "status":
-        transform_script.clean_load_status(file_path+f"{key}.csv", transform_script.dtype_status, engine)
+        transform_script.clean_load_status(file_path + f"{key}.csv", transform_script.dtype_status, engine)
     elif key == "services":
-        transform_script.clean_load_services(file_path+f"{key}.csv", transform_script.dtype_services, engine)
+        transform_script.clean_load_services(file_path + f"{key}.csv", transform_script.dtype_services, engine)
     elif key == "personnel":
-        transform_script.clean_load_personnel(file_path+f"{key}.csv", transform_script.dtype_personnel, engine)
+        transform_script.clean_load_personnel(file_path + f"{key}.csv", transform_script.dtype_personnel, engine)
