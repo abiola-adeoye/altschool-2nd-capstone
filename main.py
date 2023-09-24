765e83925d5c195572f04780bfea5a53bf413f53
abiola-adeoye <slmadeoye@gmail.com>
2023-09-24 15:36:45 +0100

    pushing logs as proof of last run

--- main.py
+++ main.py
@@ -6,9 +6,8 @@ from src.extraction_script import MHScrapper
 
 
 scraper = MHScrapper(test=False, start_page=1, stop_page=4)
-gcp_storage = MHGCPStorage('ministry-health-data-proj')
 result = scraper.scrape_mh_data()
-
+gcp_storage = MHGCPStorage('ministry-health-data-proj')
 
 for key, value in result.items():
     gcp_storage.create_or_append_json_data(key, value)
